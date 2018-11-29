0a90d195948578cc3a4e5a2aa4d019f7835de3d8
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-29 14:18:42 +0000

    moved progress bar state into coderacer container

--- coderacer/src/components/ProgressBar.js
+++ coderacer/src/components/ProgressBar.js
@@ -3,19 +3,11 @@ import { Progress } from 'semantic-ui-react'
 
 class ProgressBar extends React.Component {
 
-    state = { 
-        percent: 75 
-    }
-
-    increment = () =>
-        this.setState({
-            percent: this.state.percent >= 100 ? 0 : this.state.percent + 1,
-        })
-
     render() {
+        const { progressPercent } = this.props
         return (
             <div className="progress_bar">
-                <Progress percent={this.state.percent} indicating />
+                <Progress percent={progressPercent} indicating />
             </div>
         )
     }
