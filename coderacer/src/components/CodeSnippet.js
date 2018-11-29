4af22889407d074065bc01bf6a5ecd320415fc2f
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-29 13:35:32 +0000

    moved states and methods up a level to coderacer container

--- coderacer/src/components/CodeSnippet.js
+++ coderacer/src/components/CodeSnippet.js
@@ -5,27 +5,26 @@ import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
 
-const CodeSnippet = () => {
+const CodeSnippet = ({ code }) => {
 
+    const height = code => {
+        return (code.match(/\n/g).length + 1) * 16
+    }
 
-
-    const code = "for (var i = 0; i <= 100; i++) {\n  if (i % 2 === 0) {\n    console.log(i);\n  }\n}"
-    console.log(code.match(/\n/g).length + 1)
-        return (
-            <div className="snippet_wrapper">
-                <AceEditor
-                    height={(code.match(/\n/g).length + 1) * 16}
-                    width="100%"
-                    value={code}
-                    mode="javascript"
-                    theme="monokai"
-                    name="UNIQUE_ID_OF_DIV"
-                    editorProps={{ $blockScrolling: true }}
-                    readOnly='true'
-                />
-            </div>
-
-        )
+    return (
+        <div className="snippet_wrapper">
+            <AceEditor
+                height={height(code)}
+                width="100%"
+                value={code}
+                mode="javascript"
+                theme="monokai"
+                name="UNIQUE_ID_OF_DIV"
+                editorProps={{ $blockScrolling: true }}
+                readOnly='true'
+            />
+        </div>
+    )
 
 }
 
--- coderacer/src/containers/CodeRacerContainer.js
+++ coderacer/src/containers/CodeRacerContainer.js
@@ -4,12 +4,28 @@ import TextEditor from '../components/TextEditor';
 import ProgressBar from '../components/ProgressBar';
 
 class CodeRacerContainer extends React.Component {
+
+    state = {
+        code: "for (var i = 0; i <= 100; i++) {\n  if (i % 2 === 0) {\n    console.log(i);\n  }\n}",
+        row: 1,
+        newValue: '',
+    }
+
+    onChange = (newValue, event) => {
+        console.log(event)
+        this.setState({
+            row: event.end.row + 1,
+            newValue
+        })
+    }
     
     render() {
+        const { code, row, newValue } = this.state
+        const { onChange } = this
         return (
             <div>
-                <CodeSnippet />
-                <TextEditor />
+                <CodeSnippet code={code} />
+                <TextEditor row={row} newValue={newValue} onChange={onChange}/>
                 <ProgressBar/>
             </div>
         )
