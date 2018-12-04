8c30ae1042a2f5118127b0120fdae2cf99450720
BarneyLoosemore <biscuitfull@gmail.com>
2018-12-04 13:06:13 +0000

    login page changes

--- coderacer/src/LoginScreen.js
+++ coderacer/src/LoginScreen.js
@@ -2,6 +2,9 @@ import React from 'react'
 import SignInForm from './SignUpSignIn/SignInForm'
 import SignUpForm from './SignUpSignIn/SignUpForm'
 import LoginNav from './LoginNav'
+import gif1 from './assets/coderacer_gif_3.gif'
+import logo from './assets/coderacer_logo_new.png'
+
 
 
 class LoginScreen extends React.Component {
@@ -11,8 +14,8 @@ class LoginScreen extends React.Component {
         login: false
     }
 
-    handleRegisterClick = () => this.setState({ login: false, registration: true })
-    handleLoginClick = () => this.setState({ login: true, registration: false })
+    handleRegisterClick = () => this.setState({ login: false, registration: !this.state.registration })
+    handleLoginClick = () => this.setState({ login: !this.state.login, registration: false })
 
 
     render() {
@@ -23,7 +26,26 @@ class LoginScreen extends React.Component {
         <div>
             <LoginNav handleLoginClick={handleLoginClick} handleRegisterClick={handleRegisterClick} />
             {registration && <SignUpForm signup={signup} handleLoginClick={handleLoginClick} />}
-            {login && <SignInForm signin={signin} handleRegisterClick={handleRegisterClick} />}       
+            {login && <SignInForm signin={signin} handleRegisterClick={handleRegisterClick} />}   
+
+            
+            <img src={logo} alt={"bleh"} style={{
+                transform: "scale(0.6)",
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "50%",
+                paddingTop: "10px"
+            }}/>
+
+            <img src={gif1} alt={"bleh"} style={{    
+                transform: "scale(1)",
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "50%"
+            }} />
+            
          </div>
     )
   }
@@ -31,3 +53,4 @@ class LoginScreen extends React.Component {
 }
 
 export default LoginScreen
+
